995f8f340771d4a87b8b0c23bb13275f6549ea06
SDA_L <43444095+paps416@users.noreply.github.com>
2020-02-09 01:08:27 +0400

    Add files via upload

---
+++ well fuck/well fuck/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace well_fuck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+
+            int a, b;
+
+            Console.WriteLine("Enter first number:");
+
+            str = Console.ReadLine();
+
+            a = Convert.ToInt32(str);
+
+            Console.WriteLine("Enter second number:");
+
+            str = Console.ReadLine();
+
+            b = Convert.ToInt32(str);
+
+            int result = a + b;
+
+            Console.WriteLine("Result:" + result);
+            Console.ReadKey();
+        }
+    }
+}
